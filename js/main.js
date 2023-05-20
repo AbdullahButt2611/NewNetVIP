f20ecb6e0471f1cad227fcb5e22145319d474847
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2023-05-21 01:30:05 +0500

    Privacy Policy & Navbar

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+// Function to restrict user to enter only digits in the contact field
+
+function restrictToDigits(event) {
+    const input = event.target;
+    input.value = input.value.replace(/\D/g, '');
+}
+
+
+// Code for JavaScript
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e)=>{
+ let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+ arrowParent.classList.toggle("showMenu");
+  });
+}
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("close");
+});
