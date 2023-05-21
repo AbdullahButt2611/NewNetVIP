fce8e5c209890c813683cb63209c47140efbf91d
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2023-05-22 04:00:57 +0500

    Quantify Page

--- js/main.js
+++ js/main.js
@@ -39,37 +39,40 @@ const IconVisibility = () => {
 
 }
 
+if(btnRight && btnLeft)
+{
+    btnRight.addEventListener("click", () => {
+        tabMenu.scrollLeft += 150;
+        setTimeout(() => {
+            IconVisibility();
+        }, 50);
+    });
+    
+    
+    btnLeft.addEventListener("click", () => {
+        tabMenu.scrollLeft -= 150;
+        setTimeout(() => {
+            IconVisibility();
+        }, 50);
+    });
 
-btnRight.addEventListener("click", () => {
-    tabMenu.scrollLeft += 150;
-    setTimeout(() => {
-        IconVisibility();
-    }, 50);
-});
-
-
-btnLeft.addEventListener("click", () => {
-    tabMenu.scrollLeft -= 150;
-    setTimeout(() => {
-        IconVisibility();
-    }, 50);
-});
-
-
-window.onload = function(){
-    btnRight.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? "block" : "none";
-    btnLeft.style.display = tabMenu.scrollWidth >= window.innerWidth ? "" : "none";
+    window.onload = function(){
+        btnRight.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? "block" : "none";
+        btnLeft.style.display = tabMenu.scrollWidth >= window.innerWidth ? "" : "none";
+    }
+    
+    
+    window.onresize = function(){
+        btnRight.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? "block" : "none";
+        btnLeft.style.display = tabMenu.scrollWidth >= window.innerWidth ? "" : "none";
+    
+        let scrollLeftValue = Math.round(tabMenu.scrollLeft);
+    
+        btnLeft.style.display = scrollLeftValue > 0 ? "block" : "none";
+    }
 }
 
 
-window.onresize = function(){
-    btnRight.style.display = tabMenu.scrollWidth > tabMenu.clientWidth || tabMenu.scrollWidth >= window.innerWidth ? "block" : "none";
-    btnLeft.style.display = tabMenu.scrollWidth >= window.innerWidth ? "" : "none";
-
-    let scrollLeftValue = Math.round(tabMenu.scrollLeft);
-
-    btnLeft.style.display = scrollLeftValue > 0 ? "block" : "none";
-}
 
 
 
@@ -80,48 +83,90 @@ window.onresize = function(){
 //Javascript to make the tab navigation draggable
 
 let activeDrag = false;
-tabMenu.addEventListener("mousemove", (drag)=>{
+if(tabMenu)
+{
+    tabMenu.addEventListener("mousemove", (drag)=>{
+        
+        if(!activeDrag) return;
     
-    if(!activeDrag) return;
-
-    tabMenu.scrollLeft -= drag.movementX;
-    IconVisibility();
-    tabMenu.classList.add("dragging");
+        tabMenu.scrollLeft -= drag.movementX;
+        IconVisibility();
+        tabMenu.classList.add("dragging");
+        
+    })
     
-})
-
-tabMenu.addEventListener("mouseup", ()=>{
+    tabMenu.addEventListener("mouseup", ()=>{
+        
+        activeDrag = false;
+        tabMenu.classList.remove("dragging");
     
-    activeDrag = false;
-    tabMenu.classList.remove("dragging");
-
-})
-
-tabMenu.addEventListener("mousedown", ()=>{
+    })
     
-    activeDrag = true;
+    tabMenu.addEventListener("mousedown", ()=>{
+        
+        activeDrag = true;
+    
+    })
+}
 
-})
 //Javascript to view tab contents on click tab buttons
 const tabs = document.querySelectorAll(".tab");
 const tabBtns = document.querySelectorAll(".tab-btn");
 
-const tab_Nav = function(tabBtnClick){
+if(tabs && tabBtns){
 
-    tabBtns.forEach(tabBtn => {
-        tabBtn.classList.remove("active");
+    const tab_Nav = function(tabBtnClick){
+    
+        tabBtns.forEach(tabBtn => {
+            tabBtn.classList.remove("active");
+        });
+    
+        tabs.forEach(tab => {
+            tab.classList.remove("active");
+        });
+    
+        tabBtns[tabBtnClick].classList.add("active");
+        tabs[tabBtnClick].classList.add("active"); 
+    }
+    
+    tabBtns.forEach((tabBtn, i) => {
+        tabBtn.addEventListener("click", () => {
+            tab_Nav(i);
+        });
     });
-
-    tabs.forEach(tab => {
-        tab.classList.remove("active");
-    });
-
-    tabBtns[tabBtnClick].classList.add("active");
-    tabs[tabBtnClick].classList.add("active"); 
 }
 
-tabBtns.forEach((tabBtn, i) => {
-    tabBtn.addEventListener("click", () => {
-        tab_Nav(i);
-    });
-});
+
+
+
+
+
+
+
+
+//  =============================    Quantify Page Carousel     ==========================
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+  
